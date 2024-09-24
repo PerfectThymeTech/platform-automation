d9d1b87fd46dcac867bf5fa766dcc8b477e57518
Marvin Buss <marvin.buss@gmail.com>
2024-09-24 21:02:22 +0200

    lint

--- code/infra/modules/connectivity/networkmanager.tf
+++ code/infra/modules/connectivity/networkmanager.tf
@@ -1,13 +1,13 @@
 resource "azurerm_network_manager" "network_manager" {
-  name = "${local.prefix}-avnm"
+  name                = "${local.prefix}-avnm"
   resource_group_name = azurerm_resource_group.resource_group_network_manager.name
-  location = var.location
-  tags = var.tags
+  location            = var.location
+  tags                = var.tags
 
   description = "Global Virtual Network Manager for platform."
   scope {
     management_group_ids = [
-        var.management_group_root_id
+      var.management_group_root_id
     ]
     subscription_ids = []
   }
