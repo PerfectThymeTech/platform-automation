f092dbbe9274879d57819af1d9e9cbe1d98d1c9d
Marvin Buss <marvin.buss@gmail.com>
2024-09-24 16:22:58 +0200

    lint

--- code/infra/modules/policy_role_assignments/main.tf
+++ code/infra/modules/policy_role_assignments/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "policy_role_assignment" {
-  for_each           = local.role_definition_ids_map
+  for_each = local.role_definition_ids_map
 
   description        = "Role definition required for Azure Policy."
   principal_id       = var.policy_assignment_principal_id
