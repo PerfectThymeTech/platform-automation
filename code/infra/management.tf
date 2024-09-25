92ce43c937a60800bca2851826ffad2c4986d22f
Marvin Buss <marvin.buss@gmail.com>
2024-09-25 15:55:53 +0200

    Add missing input

--- code/infra/management.tf
+++ code/infra/management.tf
@@ -4,8 +4,9 @@ module "management" {
     azurerm = azurerm.management
   }
 
-  location    = var.location
-  environment = var.environment
-  prefix      = var.prefix
-  tags        = var.tags
+  location                 = var.location
+  environment              = var.environment
+  prefix                   = var.prefix
+  tags                     = var.tags
+  management_group_root_id = azurerm_management_group.management_group_root.id
 }
